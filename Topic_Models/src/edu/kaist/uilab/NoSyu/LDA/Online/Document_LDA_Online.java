9ce714beefafbf7265b46bad0944c03e669a88a3
JinYeong Bak <dongdm@gmail.com>
2013-10-21 16:02:29 +0800

    Delete previous one and change it to new one

--- Topic_Models/src/edu/kaist/uilab/NoSyu/LDA/Online/Document_LDA_Online.java
+++ Topic_Models/src/edu/kaist/uilab/NoSyu/LDA/Online/Document_LDA_Online.java
@@ -1,20 +1,19 @@
 package edu.kaist.uilab.NoSyu.LDA.Online;
 
-import java.util.Random;
+import java.util.Map.Entry;
 import java.util.TreeSet;
 
-import org.ejml.simple.SimpleMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 
 import edu.kaist.uilab.NoSyu.utils.Document;
-import edu.kaist.uilab.NoSyu.utils.Matrix_Functions;
+import edu.kaist.uilab.NoSyu.utils.Matrix_Functions_ACM3;
 
 /*
  * A document
  * */
 public class Document_LDA_Online extends Document
 {
-	public SimpleMatrix ss_lambda = null;	// n_sw * phi_swk === phi_skw
-	public SimpleMatrix gamma_sk = null;
+	public ArrayRealVector gamma_sk = null;
 	
 	/*
 	 * Constructor
@@ -27,10 +26,18 @@ public class Document_LDA_Online extends Document
 	/*
 	 * To save the memory, Initialize the parameter value at this time, not construction time
 	 * */
-	public void Start_this_document(int TopicNum, Random rand)
+	public void Start_this_document(int TopicNum)
 	{
-		this.gamma_sk = new SimpleMatrix(1, TopicNum);
-		Matrix_Functions.SetGammaDistribution(this.gamma_sk, 100.0, 0.01);
+		int target_filename = Integer.parseInt(this.filename) - 1;
+		if(target_filename < 128)
+		{
+			this.gamma_sk = Matrix_Functions_ACM3.load_vector_txt("init_gammad_" + target_filename + ".txt");
+		}
+		else
+		{
+			this.gamma_sk = new ArrayRealVector(TopicNum);
+			Matrix_Functions_ACM3.SetGammaDistribution(this.gamma_sk, 100.0, 0.01);
+		}
 	}
 	
 	/*
@@ -38,26 +45,48 @@ public class Document_LDA_Online extends Document
 	 * */
 	public void Set_Null()
 	{
-//		this.phi_swk = null;
 		this.gamma_sk = null;
 	}
 	
 	
-	public SimpleMatrix get_word_freq_vector()
+	public ArrayRealVector get_word_freq_vector()
 	{
 		int VocaNum_for_this_document = this.get_voca_cnt();
 		
-		SimpleMatrix output_matrix = new SimpleMatrix(1, VocaNum_for_this_document);
+		ArrayRealVector output_vector = new ArrayRealVector(VocaNum_for_this_document);
 		
 		TreeSet<Integer> voca_idx_set = this.get_voca_index();
 		int matrix_col_idx = 0;
 		
 		for(Integer voca_idx : voca_idx_set)
 		{
-			output_matrix.set(0, matrix_col_idx, (double)this.number_dv.get(voca_idx));
+			output_vector.setEntry(matrix_col_idx, (double)this.number_dv.get(voca_idx));
 			matrix_col_idx++;
 		}
 		
-		return output_matrix;
+		return output_vector;
+	}
+	
+	
+	public int[] get_real_voca_index_array_sorted()
+	{
+		int VocaNum_for_this_document = this.get_voca_cnt();
+		int[] voca_for_this_doc_index_array = new int[VocaNum_for_this_document];
+		
+		TreeSet<Integer> real_voca_idx_set = new TreeSet<Integer>();
+		
+		for(Entry<Integer, Integer> one_entry : voca_idx_to_real_voca_idx.entrySet())
+		{
+			real_voca_idx_set.add(one_entry.getValue());
+		}
+		
+		int arr_idx = 0;
+		for(int one_real_voca_idx : real_voca_idx_set)
+		{
+			voca_for_this_doc_index_array[arr_idx] = one_real_voca_idx;
+			arr_idx++;
+		}
+		
+		return voca_for_this_doc_index_array;
 	}
 }
--- Topic_Models/src/edu/kaist/uilab/NoSyu/LDA/Online/oLDA_Main.java
+++ Topic_Models/src/edu/kaist/uilab/NoSyu/LDA/Online/oLDA_Main.java
@@ -6,13 +6,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
 
-import org.ejml.simple.SimpleMatrix;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.special.Gamma;
+import org.apache.commons.math3.util.FastMath;
 
-import edu.kaist.uilab.NoSyu.utils.Matrix_Functions;
+import edu.kaist.uilab.NoSyu.utils.Matrix_Functions_ACM3;
 import edu.kaist.uilab.NoSyu.utils.Miscellaneous_function;
 
 public class oLDA_Main 
@@ -24,18 +25,16 @@ public class oLDA_Main
 	private static int Max_Iter;	// Maximum number of iteration for E_Step
 	private static double convergence_limit = 0.001;
 	
-	private static Random rand;	// Random object
+	private static Array2DRowRealMatrix Lambda_kv;				// lambda
+	private static Array2DRowRealMatrix Expectation_Lambda_kv;				// Expectation of log beta
 	
-	private static SimpleMatrix Lambda_kv;				// lambda
-	private static SimpleMatrix Expectation_Lambda_kv;				// Expectation of log beta
-	
-	private static SimpleMatrix alpha;		// Hyper-parameter for theta
+	private static ArrayRealVector alpha;	// Hyper-parameter for theta
 	private static double eta = 0.01;		// Hyper-parameter for beta
-	private static double tau0 = 1024.0;
+	private static double tau0 = 1025.0;
 	private static double kappa = 0.7;
 	private static double update_t;
 	private static double rho_t;
-	private static int minibatch_size = 1;
+	private static int minibatch_size;
 	
 	private static String voca_file_path = null;
 	private static String BOW_file_path = null;
@@ -45,6 +44,12 @@ public class oLDA_Main
 	private static ArrayList<String> Vocabulary_list;	// vocabulary
 	
 	private static int max_rank = 30;
+	
+	private static int[] topic_index_array = null;
+	
+	private static double sum_score;
+	private static double sum_word_count;
+	private static double loggamma_sum_alpha;
 	
 	public static void main(String[] args) 
 	{
@@ -58,37 +63,53 @@ public class oLDA_Main
 		// Variables
 		int start_doc_idx = 0;
 		int last_doc_idx = minibatch_size;
-		boolean is_run_end = false;
+		double minibatch_size_this_iter = 0;
+		Array2DRowRealMatrix sumed_ss_lambda = null;
 		
 		// Run oLDA
 		while(true)
 		{
 			// Start
 			minibatch_document_list = document_list.subList(start_doc_idx, last_doc_idx);
+			minibatch_size_this_iter = (double)(minibatch_document_list.size());
+			sum_score = 0;
+			sum_word_count = 0;
 			
 			// E step
-			SimpleMatrix sumed_ss_lambda = E_Step(minibatch_document_list);
+			sumed_ss_lambda = E_Step(minibatch_document_list);
+			
+			// Print perplexity
+			System.out.println("Perplexity:\t" + Compute_perplexity(minibatch_size_this_iter));
 			
 			// M step
-			M_Step(sumed_ss_lambda, (double)(minibatch_document_list.size()));
-			
-			// Print perplexity or likelihood
+			M_Step(sumed_ss_lambda, minibatch_size_this_iter);
 			
 			// Update index variable
-			if(true == is_run_end)
-			{
-				break;
-			}
-			
+			update_t++;
 			start_doc_idx = last_doc_idx;
 			last_doc_idx += minibatch_size;
-			if(last_doc_idx > DocumentNum)
+			
+			if(last_doc_idx >= DocumentNum)
 			{
 				last_doc_idx = (int)DocumentNum;
-				is_run_end = true;
+				
+				// Start
+				minibatch_document_list = document_list.subList(start_doc_idx, last_doc_idx);
+				minibatch_size_this_iter = (double)(minibatch_document_list.size());
+				sum_score = 0;
+				sum_word_count = 0;
+				
+				// E step
+				sumed_ss_lambda = E_Step(minibatch_document_list);
+				
+				// Print perplexity
+				System.out.println("Perplexity:\t" + Compute_perplexity(minibatch_size_this_iter));
+				
+				// M step
+				M_Step(sumed_ss_lambda, minibatch_size_this_iter);
+				
+				break;
 			}
-			
-			update_t++;
 		}
 		
 		// Print result
@@ -113,16 +134,22 @@ public class oLDA_Main
 			Vocabulary_list = Miscellaneous_function.makeStringListFromFile(voca_file_path);
 			VocaNum = Vocabulary_list.size();
 			
-			rand = new Random(1);
 			update_t = 0;
 			
-			alpha = new SimpleMatrix(1, TopicNum);
-			alpha.set(0.01);
+			alpha = new ArrayRealVector(TopicNum, 0.01);
+			loggamma_sum_alpha = Gamma.logGamma(Matrix_Functions_ACM3.Fold_Vec(alpha));
 			
-			Lambda_kv = new SimpleMatrix(TopicNum, VocaNum);
-			Matrix_Functions.SetGammaDistribution(Lambda_kv, 100.0, 0.01);
+			Lambda_kv = new Array2DRowRealMatrix(TopicNum, VocaNum);
+			Matrix_Functions_ACM3.SetGammaDistribution(Lambda_kv, 100.0, 0.01);
+//			Lambda_kv = Matrix_Functions_ACM3.load_matrix_txt("lambda_init.txt");
 			
-			Expectation_Lambda_kv = Matrix_Functions.Compute_Dirichlet_Expectation_col(Lambda_kv);
+			Expectation_Lambda_kv = Matrix_Functions_ACM3.Compute_Dirichlet_Expectation_col(Lambda_kv);
+			
+			topic_index_array = new int[TopicNum];
+			for(int idx = 0 ; idx < TopicNum ; idx++)
+			{
+				topic_index_array[idx] = idx;
+			}
 		}
 		catch(java.lang.Throwable t)
 		{
@@ -165,41 +192,37 @@ public class oLDA_Main
 	
 	/*
 	 * Run E step
-	 * 	Document_LDA_Online target_document	- target document instance 
+	 * 	Document_LDA_Online_ACM3 target_document	- target document instance 
 	 * 	SimpleMatrix doc_Expe_Lambda_kv		- Expectation of log beta which matches vocabulary index in this document, K x V' 
 	 * */
-	private static SimpleMatrix E_Step(List<Document_LDA_Online> minibatch_document_list)
+	private static Array2DRowRealMatrix E_Step(List<Document_LDA_Online> minibatch_document_list)
 	{
 		// Set delta lambda
-		SimpleMatrix sumed_ss_lambda = new SimpleMatrix(TopicNum, VocaNum);
-		SimpleMatrix temp_col_vec = null;
+		Array2DRowRealMatrix sumed_ss_lambda = new Array2DRowRealMatrix(TopicNum, VocaNum);
+		Array2DRowRealMatrix ss_lambda_for_one_doc = null;
 		int real_voca_idx = 0;
-		
+		int[] voca_for_this_doc_index_array = null;
+
 		for(Document_LDA_Online one_doc : minibatch_document_list)
 		{
-			System.out.println("Document Name:\t" + one_doc.get_filename());
+//			System.out.println("Document Name:\t" + one_doc.get_filename());
 			
-			int VocaNum_for_this_document = one_doc.get_voca_cnt();
-			SimpleMatrix doc_Expe_Lambda_kv = new SimpleMatrix(TopicNum, VocaNum_for_this_document);	// K x V'
+			voca_for_this_doc_index_array = one_doc.get_real_voca_index_array_sorted();
 			
-			// Get Expectation_Lambda_kv which matches vocabulary index in this document
-			HashMap<Integer, Integer> voca_idx_to_real_voca_idx = one_doc.get_voca_idx_to_real_voca_idx();
-			for(int doc_Lambda_kv_col_idx = 0 ; doc_Lambda_kv_col_idx < VocaNum_for_this_document ; doc_Lambda_kv_col_idx++)
-			{
-				real_voca_idx = voca_idx_to_real_voca_idx.get(doc_Lambda_kv_col_idx);
-				doc_Expe_Lambda_kv.insertIntoThis(0, doc_Lambda_kv_col_idx, Expectation_Lambda_kv.extractVector(false, real_voca_idx));
-			}
+			Array2DRowRealMatrix doc_Expe_Lambda_kv = (Array2DRowRealMatrix) Expectation_Lambda_kv.getSubMatrix(topic_index_array, voca_for_this_doc_index_array);
 			
 			// E step for this document
-			E_Step_for_one_doc(one_doc, doc_Expe_Lambda_kv);
+			ss_lambda_for_one_doc = E_Step_for_one_doc(one_doc, doc_Expe_Lambda_kv);
 			
 			// Summed ss_lambda
+			int VocaNum_for_this_document = one_doc.get_voca_cnt();
 			for(int sumed_ss_lambda_col_idx = 0 ; sumed_ss_lambda_col_idx < VocaNum_for_this_document ; sumed_ss_lambda_col_idx++)
 			{
-				real_voca_idx = voca_idx_to_real_voca_idx.get(sumed_ss_lambda_col_idx);
-				temp_col_vec = sumed_ss_lambda.extractVector(false, real_voca_idx);
-				temp_col_vec = temp_col_vec.plus(one_doc.ss_lambda.extractVector(false, sumed_ss_lambda_col_idx));
-				sumed_ss_lambda.insertIntoThis(0, real_voca_idx, temp_col_vec);
+				real_voca_idx = voca_for_this_doc_index_array[sumed_ss_lambda_col_idx];
+				for(int row_idx = 0 ; row_idx < TopicNum ; row_idx++)
+				{
+					sumed_ss_lambda.addToEntry(row_idx, real_voca_idx, ss_lambda_for_one_doc.getEntry(row_idx, sumed_ss_lambda_col_idx));
+				}
 			}
 		}
 		
@@ -209,52 +232,75 @@ public class oLDA_Main
 	
 	/*
 	 * Run E step for one document
-	 * 	Document_LDA_Online target_document	- target document instance 
+	 * 	Document_LDA_Online_ACM3 target_document	- target document instance 
 	 * 	SimpleMatrix doc_Expe_Lambda_kv		- Expectation of log beta which matches vocabulary index in this document, K x V' 
 	 * */
-	private static void E_Step_for_one_doc(Document_LDA_Online target_document, SimpleMatrix doc_Expe_Lambda_kv)
+	private static Array2DRowRealMatrix E_Step_for_one_doc(Document_LDA_Online target_document, Array2DRowRealMatrix doc_Expe_Lambda_kv)
 	{
 		// Iteration
-		target_document.Start_this_document(TopicNum, rand);
+		target_document.Start_this_document(TopicNum);
 		
 		double changes_gamma_s = 0.0;
-		SimpleMatrix temp_matrix = null;
-		SimpleMatrix phi_skw = null;
-		SimpleMatrix n_sw_phi_skw = null;
+		ArrayRealVector temp_vector = null;
+		Array2DRowRealMatrix phi_skw = null;
+		Array2DRowRealMatrix n_sw_phi_skw = null;
+		ArrayRealVector word_freq_vector = target_document.get_word_freq_vector();
 		
 		for (int iter_idx = 0 ; iter_idx < Max_Iter ; iter_idx++)
 		{
 			// Update phi
-			temp_matrix = Matrix_Functions.Compute_Dirichlet_Expectation_col(target_document.gamma_sk);	// temp_matrix = E_q [log \thtea_sk], 1 x K
-			phi_skw = Matrix_Functions.Sum_Matrix_col_vector(doc_Expe_Lambda_kv, temp_matrix.transpose());	// K x V'
-			Matrix_Functions.Do_Exponential(phi_skw);
-			Matrix_Functions.Col_Normalization(phi_skw);	// K x V'
+			temp_vector = Matrix_Functions_ACM3.Compute_Dirichlet_Expectation(target_document.gamma_sk);	// temp_matrix = E_q [log \thtea_sk], 1 x K
+			phi_skw = Matrix_Functions_ACM3.Sum_Matrix_col_vector(doc_Expe_Lambda_kv, temp_vector);	// K x V'
+			Matrix_Functions_ACM3.Do_Exponential(phi_skw);
+			Matrix_Functions_ACM3.Col_Normalization(phi_skw);	// K x V'
 			
 			// Update gamma
-			n_sw_phi_skw = Matrix_Functions.Mul_Matrix_row_vector(phi_skw, target_document.get_word_freq_vector());	// K x V'
-			temp_matrix = Matrix_Functions.Fold_Col(n_sw_phi_skw).transpose();	// 1 x K
-			temp_matrix = alpha.plus(temp_matrix);								// 1 x K
-			changes_gamma_s = Matrix_Functions.Diff_Two_Matrix(temp_matrix, target_document.gamma_sk);	// Get changes
-			target_document.gamma_sk = temp_matrix;	// Assign
+			n_sw_phi_skw = Matrix_Functions_ACM3.Mul_Matrix_row_vector(phi_skw, word_freq_vector);	// K x V'
+			temp_vector = Matrix_Functions_ACM3.Fold_Col(n_sw_phi_skw);	// 1 x K
+			temp_vector = alpha.add(temp_vector);
+			changes_gamma_s = Matrix_Functions_ACM3.Diff_Two_Vector(temp_vector, target_document.gamma_sk);	// Get changes
+			target_document.gamma_sk = temp_vector;	// Assign
 			
 			// Check convergence
-			if(changes_gamma_s < convergence_limit)
+			if((changes_gamma_s / TopicNum) < convergence_limit)
 			{
 				break;
 			}
 		}
 		
-		target_document.ss_lambda = n_sw_phi_skw;	// K x V'
+		// Compute for perplexity
+		int VocaNum_for_this_document = target_document.get_voca_cnt();
+		ArrayRealVector log_theta_sk_vec = Matrix_Functions_ACM3.Compute_Dirichlet_Expectation(target_document.gamma_sk);	// temp_matrix = E_q [log \thtea_sk], 1 x K
+		double score_this_doc = 0;
+		double phinorm = 0;
+		
+		for(int col_idx = 0 ; col_idx < VocaNum_for_this_document ; col_idx++)
+		{
+			temp_vector = log_theta_sk_vec.add(doc_Expe_Lambda_kv.getColumnVector(col_idx));
+			phinorm = Matrix_Functions_ACM3.Vec_Exp_Normalization_with_Log(temp_vector);
+			score_this_doc += phinorm * word_freq_vector.getEntry(col_idx);
+		}
+		sum_score += score_this_doc;
+		
+		temp_vector = Matrix_Functions_ACM3.elementwise_mul_two_vector(alpha.subtract(target_document.gamma_sk), log_theta_sk_vec);
+		sum_score += Matrix_Functions_ACM3.Fold_Vec(temp_vector);
+		
+		temp_vector = Matrix_Functions_ACM3.Do_Gammaln_return(target_document.gamma_sk);
+		temp_vector = temp_vector.subtract(Matrix_Functions_ACM3.Do_Gammaln_return(alpha));
+		sum_score += Matrix_Functions_ACM3.Fold_Vec(temp_vector);
+		
+		sum_score += loggamma_sum_alpha - Gamma.logGamma(Matrix_Functions_ACM3.Fold_Vec(target_document.gamma_sk));
+		
+		sum_word_count += Matrix_Functions_ACM3.Fold_Vec(word_freq_vector);
+		
+		return n_sw_phi_skw;	// K x V'
 	}
 	
 	/*
 	 * Run M Step
 	 * */
-	private static void M_Step(SimpleMatrix sumed_ss_lambda, double minibatch_size_now)
+	private static void M_Step(Array2DRowRealMatrix sumed_ss_lambda, double minibatch_size_now)
 	{
-		SimpleMatrix temp_1 = null;
-		SimpleMatrix temp_2 = null;
-		
 		// Compute rho_t
 		rho_t = Math.pow(tau0 + update_t, -kappa);
 		if(rho_t < 0.0)
@@ -262,19 +308,16 @@ public class oLDA_Main
 			rho_t = 0.0;
 		}
 		
-		sumed_ss_lambda = sumed_ss_lambda.scale(DocumentNum / minibatch_size_now);	// K x V
-		SimpleMatrix delta_lambda = new SimpleMatrix(TopicNum, VocaNum);
-		delta_lambda.set(eta);
-		
-		delta_lambda = delta_lambda.plus(sumed_ss_lambda);
+		sumed_ss_lambda = (Array2DRowRealMatrix) sumed_ss_lambda.scalarMultiply(DocumentNum / minibatch_size_now);	// K x V
+		Array2DRowRealMatrix delta_lambda = (Array2DRowRealMatrix) sumed_ss_lambda.scalarAdd(eta);
 		
 		// Update lambda
-		temp_1 = Lambda_kv.scale(1.0 - rho_t);
-		temp_2 = delta_lambda.scale(rho_t);
-		Lambda_kv = temp_1.plus(temp_2);	// K x V
-
+		Array2DRowRealMatrix temp_1 = (Array2DRowRealMatrix) Lambda_kv.scalarMultiply(1.0 - rho_t);
+		Array2DRowRealMatrix temp_2 = (Array2DRowRealMatrix) delta_lambda.scalarMultiply(rho_t);
+		Lambda_kv = temp_1.add(temp_2);	// K x V
+		
 		// Compute Expectation_Lambda_kv
-		Expectation_Lambda_kv = Matrix_Functions.Compute_Dirichlet_Expectation_col(Lambda_kv);
+		Expectation_Lambda_kv = Matrix_Functions_ACM3.Compute_Dirichlet_Expectation_col(Lambda_kv);
 	}
 	
 	
@@ -285,7 +328,7 @@ public class oLDA_Main
 	{
 		try
 		{
-			SimpleMatrix temp_row_vec = null;
+			double[] temp_row_vec = null;
 			int[] sorted_idx = null;
 			
 			PrintWriter lambda_out = new PrintWriter(new FileWriter(new File("oLDA_result_" + output_file_name + "_topic_voca_30.csv")));
@@ -293,7 +336,7 @@ public class oLDA_Main
 			// Lambda with Rank
 			for(int topic_idx = 0 ; topic_idx < TopicNum ; topic_idx++)
 			{
-				temp_row_vec = Lambda_kv.extractVector(true, topic_idx);
+				temp_row_vec = Lambda_kv.getRow(topic_idx);
 				sorted_idx = Miscellaneous_function.Sort_Ranking_Double(temp_row_vec, max_rank);
 				
 				lambda_out.print(topic_idx);
@@ -307,7 +350,7 @@ public class oLDA_Main
 			lambda_out.close();
 			
 			// Lambda
-			Lambda_kv.saveToFileCSV("oLDA_result_" + output_file_name + "_lambda_kv.csv");
+			Matrix_Functions_ACM3.saveToFileCSV(Lambda_kv, "oLDA_result_" + output_file_name + "_lambda_kv.csv");
 		}
 		catch(java.lang.Throwable t)
 		{
@@ -315,5 +358,33 @@ public class oLDA_Main
 			t.printStackTrace();
 			System.exit(1);
 		}
+	}
+	
+	
+	/*
+	 * Compute perplexity
+	 * */
+	private static double Compute_perplexity(double minibatch_size_now)
+	{
+		// Compute for perplexity
+		sum_score *= DocumentNum / minibatch_size_now;
+		
+		Array2DRowRealMatrix temp_matrix = (Array2DRowRealMatrix) Lambda_kv.scalarMultiply(-1).scalarAdd(eta);
+		temp_matrix = Matrix_Functions_ACM3.elementwise_mul_two_matrix(temp_matrix, Expectation_Lambda_kv);
+		sum_score += Matrix_Functions_ACM3.Fold_Matrix(temp_matrix);
+		
+		temp_matrix = Matrix_Functions_ACM3.Do_Gammaln_return(Lambda_kv);
+		temp_matrix = (Array2DRowRealMatrix) temp_matrix.scalarAdd(-Gamma.logGamma(eta));
+		sum_score += Matrix_Functions_ACM3.Fold_Matrix(temp_matrix);
+		
+		ArrayRealVector temp_vector = Matrix_Functions_ACM3.Fold_Col(Lambda_kv);
+		temp_vector = Matrix_Functions_ACM3.Do_Gammaln_return(temp_vector);
+		temp_vector.mapMultiplyToSelf(-1);
+		temp_vector.mapAddToSelf(Gamma.logGamma(eta * VocaNum));
+		sum_score += Matrix_Functions_ACM3.Fold_Vec(temp_vector);
+		
+		double perwordbound = sum_score * minibatch_size_now / (DocumentNum * sum_word_count);
+		
+		return FastMath.exp(-perwordbound);		
 	}
 }
